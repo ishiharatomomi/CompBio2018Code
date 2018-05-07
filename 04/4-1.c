7c59d65adf093137f4c97f03886b5db17145bcc9
ishiharatomomi <honeybeepooh83@gmail.com>
2018-05-07 16:25:45 +0900

    2018年5月7日　１

---
+++ 04/4-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+int i;
+int a[10];
+
+for (i=0;i<10;i++) {
+   a[i]=i;
+}
+
+for (i=0;i<10;i++){
+printf("%d\n",a[i]);
+}
+  return 0;
+}
---
+++ 04/4-4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(int argc, char const *argv[]) {
+int i;
+double d;
+
+//int -> double
+i=5;
+d=i;
+printf("%d,%f\n",i,d);
+
+//double->int
+d=3.6;
+i=d;
+printf("%d %f\n",i,d);
+
+  return 0;
+}
